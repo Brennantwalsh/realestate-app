8331d76f3d8c94a877c3e1776834a3184af0ee15
Brennantwalsh <brennanthomaswalsh@gmail.com>
2016-09-09 00:28:20 -0500

    initial

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+House.create!([
+  {description: "First House", year_built: 1995, square_feet: 50, bedrooms: 2, bathrooms: 1, availability: true, price: 67.0},
+  {description: "The last of all the houses", year_built: 1994, square_feet: 100, bedrooms: 1, bathrooms: 4, availability: false, price: 800.0}
+])
